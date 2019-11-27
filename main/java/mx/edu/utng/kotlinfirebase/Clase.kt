1c2fdd84f760920e881dbc0435b664b676ffe726
JoelPalomares <49220677+JoelPalomares@users.noreply.github.com>
2019-11-26 23:28:57 -0600

    Add files via upload

---
+++ main/java/mx/edu/utng/kotlinfirebase/Clase.kt
@@ -0,0 +1,9 @@
+package mx.edu.utng.kotlinfirebase
+
+class Clase (
+     val claseId: String? = null,
+     val carrera:String? = null,
+     val materia:String? = null,
+     val tema:String? = null
+)
+
